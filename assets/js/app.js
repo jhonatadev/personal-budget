74d40ea4db67ea2c1dee4227ee69cdf67b9b6ebf
jhonatadev <xxxonata@gmail.com>
2019-07-01 00:19:45 -0300

    Importação inicial :)

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+function cadastrarDespesa() {
+  let ano = document.getElementById('ano')
+  let mes = document.getElementById('mes')
+  let dia = document.getElementById('dia')
+  let tipo = document.getElementById('tipo')
+  let descricao = document.getElementById('descricao')
+  let valor = document.getElementById('valor')
+
+  console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+}
