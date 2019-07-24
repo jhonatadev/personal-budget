3445978b6446591916b7648551c9a5c7a3253b2e
jhonatadev <xxxonata@gmail.com>
2019-07-23 23:39:39 -0300

    Validando dados antes do registro

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,15 @@ class Despesa {
     this.descricao = descricao
     this.valor = valor
   }
+
+  validarDados() {
+    for (let i in this) {
+      if (this[i] == undefined || this[i] == '' || this[i] === null) {
+        return false
+      }
+    }
+    return true
+  }
 }
 
 class Bd {
@@ -49,5 +58,12 @@ function cadastrarDespesa() {
     valor.value
   )
 
-  bd.gravar(despesa)
+  if(despesa.validarDados()){
+    bd.gravar(despesa)
+    // Dialog de sucesso
+    $('#sucessoGravacao').modal('show')
+  } else {
+    // Dialog de erro
+      $('#erroGravacao').modal('show')
+   }
 }
