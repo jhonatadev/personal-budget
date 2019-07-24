56a6bffe99d9039381a472da75e52a60dccbdca9
jhonatadev <xxxonata@gmail.com>
2019-07-23 21:44:29 -0300

    Criando a Class Despesa

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,14 @@
+class Despesa {
+  constructor(ano, mes, dia, tipo, descricao, valor) {
+    this.ano = ano
+    this.mes = mes
+    this.dia = dia
+    this.tipo = tipo
+    this.descricao = descricao
+    this.valor = valor
+  }
+}
+
 function cadastrarDespesa() {
   let ano = document.getElementById('ano')
   let mes = document.getElementById('mes')
@@ -6,5 +17,14 @@ function cadastrarDespesa() {
   let descricao = document.getElementById('descricao')
   let valor = document.getElementById('valor')
 
-  console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+  let despesa = new Despesa (
+    ano.value, 
+    mes.value, 
+    dia.value, 
+    tipo.value, 
+    descricao.value, 
+    valor.value
+  )
+
+  console.log(despesa)
 }
