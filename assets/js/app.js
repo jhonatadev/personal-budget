dfc165ea050a8f574b0a465207992a3a2b4cf05f
jhonatadev <xxxonata@gmail.com>
2019-07-26 05:19:21 -0300

    Removendo despesas

--- assets/js/app.js
+++ assets/js/app.js
@@ -56,6 +56,7 @@ class Bd {
         continue
       }
 
+      despesa.id = i
       despesas.push(despesa)
     }
     return despesas
@@ -92,6 +93,10 @@ class Bd {
     }
 
     return despesasFiltradas
+  }
+
+  remover(id) {
+  	localStorage.removeItem(id)
   }
 }
 
@@ -178,7 +183,26 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
 
     linha.insertCell(1).innerHTML = d.tipo
     linha.insertCell(2).innerHTML = d.descricao 
-    linha.insertCell(3).innerHTML = d.valor  
+    linha.insertCell(3).innerHTML = d.valor 
+
+	  // Criar o botão de exclusão
+	  let btn = document.createElement("button")
+	  btn.className = 'btn btn-danger'
+	  btn.innerHTML = '<i class="fas fa-trash"></i>'
+	  btn.id = `id_despesa_${d.id}`
+
+	  btn.onclick =function() {
+	  	let trash = confirm(`Deseja realmente excluir a despesa ${d.tipo}?`) 
+	  	
+	  	if(trash == true) {
+		  	// Remover a despesa
+		  	let id = this.id.replace('id_despesa_', '')
+	  		bd.remover(id)
+	  		window.location.reload()
+	  	}
+	  }
+
+	  linha.insertCell(4).append(btn)
   })
 }
 
